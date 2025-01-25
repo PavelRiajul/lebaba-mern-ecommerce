732a66526a0497f31c8ac554d9cc843028162eaf
Riajul Islam <riajulpislam@gmail.com>
2025-01-25 20:32:18 +0600

    new Trends Section

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "./Banner"
 import Categories from "./Categories"
+import Trends from "./Trends"
 
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <Banner/>
       <Categories/>
+      <Trends/>
     </div>
   )
 }
