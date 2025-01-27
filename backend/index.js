fa7689d3d61d767cc08741a2a3dd728661b762c2
Riajul Islam <riajulpislam@gmail.com>
2025-01-27 12:42:48 +0600

    install mongoose dot dev and mongodb database create

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;  //localhost:5000 port a run hobe
+const mongoose = require('mongoose');    //mongoose import
+require('dotenv').config()
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);  //mongodb database connection url .env file
+}
+main().then(()=> console.log("Mongodb connected successfully")).catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('lebabae-commerce!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
