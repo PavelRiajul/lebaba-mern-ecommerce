6d96a9586bc5a4e21b28f151fd715459a56f16a4
Riajul Islam <riajulpislam@gmail.com>
2025-01-28 22:37:13 +0600

    get single product update and delete product routes

--- backend/src/products/product.controller.js
+++ backend/src/products/product.controller.js
@@ -60,7 +60,62 @@ const getAllProducts =async(req,res)=>{
         return errorResponse(res,500,"Failed to get all products",error)
     }
 }
+//gets single product
+const getSingleProduct =async (req,res)=>{
+    const {id} = req.params;
+    try {
+        const  product = await Products.findById(id).populate('author','username,email')
+        if(!product){
+            return errorResponse(res,404,"Product not found")
+        }
+        const reviews = await Reviews.find({productId:id}).populate('userId','username email')
+        return successResponse(res,200 ,"Single Product and reviews",{product ,reviews})
+    } catch (error) {
+        return errorResponse(res,500,"Failed to get single product",error)
+
+    }
+}
+
+// update product (admin only)
+const updateProductById= async(req,res)=>{
+    const productId = req.params.id;
+    try {
+        const updateProduct = await Products.findByIdAndUpdate(productId,{...req.body},{
+              new:true
+        })
+        if(!updateProduct){
+            return errorResponse(res,404,"Product not found")
+        }
+        return successResponse(res,200,"Product update successfully!",updateProduct)
+    } catch (error) {
+        return errorResponse(res,500,"Failed to update",error)
+    }        
+
+}
+
+//delete product
+const deleteProductById=async(req,res)=>{
+       const productId = req.params.id;
+       try {
+        const deleteProduct = await Products.findByIdAndDelete(productId)
+
+        if(!deleteProduct){
+            return errorResponse(res,404,"Product not found")
+        }
+        await Reviews.deleteOne({productId:productId})
+        return successResponse(res,200,"Product deleted successfully!")
+
+       } catch (error) {
+        return errorResponse(res,500,"Failed to delete",error)
+
+       }
+}
+
+
 module.exports = {
     createNewProduct,
-    getAllProducts
+    getAllProducts,
+    getSingleProduct,
+    updateProductById,
+    deleteProductById
 }
--- backend/src/products/product.route.js
+++ backend/src/products/product.route.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { createNewProduct, getAllProducts } = require('./product.controller')
+const { createNewProduct, getAllProducts, getSingleProduct, updateProductById, deleteProductById } = require('./product.controller')
 const verifyToken = require('../middleware/verifyToken')
 const verifyAdmin = require('../middleware/verifyAdmin')
 const router = express.Router()
@@ -9,5 +9,14 @@ router.post("/create-product",createNewProduct)
 
 //router all products
 router.get('/',getAllProducts)
+
+//get single product
+router.get('/:id',getSingleProduct)
+
+//update product (admin only)
+router.patch("/update-product/:id",verifyToken,verifyAdmin,updateProductById)
+
+//delete product (admin only)
+router.delete("/:id",verifyToken,verifyAdmin,deleteProductById)
 
 module.exports = router
