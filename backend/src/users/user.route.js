866bf573609b40e29fb2796155c277e7c53b7f90
Riajul Islam <riajulpislam@gmail.com>
2025-01-28 17:25:58 +0600

    authorization and authentication verify token and admin middleware

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { userRegistration, userLoggedIn, userLogout, getAllUsers } = require('./user.controller');
+const verifyToken = require('../middleware/verifyToken');
+const verifyAdmin = require('../middleware/verifyAdmin');
 const router = express.Router();
 
 //register endpoint
@@ -11,5 +13,5 @@ router.post('/login',userLoggedIn)
 //logout endpoint
 router.post('/logout',userLogout)
 //get all users endpoints (token verify and admin)
-router.get('/users',getAllUsers)
+router.get('/users',verifyToken,verifyAdmin,getAllUsers)
 module.exports=router;
