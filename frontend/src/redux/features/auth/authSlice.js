30df3af16154dfc0bdbff2562758984c70ec6873
Riajul Islam <riajulpislam@gmail.com>
2025-01-28 12:52:58 +0600

    create authslice to manage user state

---
+++ frontend/src/redux/features/auth/authSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const loadUserFromLocalStorage=()=>{
+    try {
+        const serializedState = localStorage.getItem('user');
+        if(serializedState === null) return {user:null}
+        return {user:JSON.parse(serializedState)}
+    } catch (error) {
+        return {user:null}
+    }
+}
+
+const initialState= loadUserFromLocalStorage()
+const authSlice = createSlice({
+    name:'auth',
+    initialState,
+})
