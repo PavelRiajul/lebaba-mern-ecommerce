ee9231f909b897d2564629c487f8c05c002af031
Riajul Islam <riajulpislam@gmail.com>
2025-01-28 14:29:01 +0600

    show dropdown menus based on user and admin role

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,31 @@
+import { useSelector } from "react-redux"
 import { Link, NavLink } from "react-router"
-
+import avatarImg from "../../assets/avatar.png"
+import { useState } from "react"
 
 const Navbar = () => {
+  const {user} = useSelector((state)=> state.auth)  //login korle user show dekhabe
+  //login korle show dropdown menu show korbe user navigation manu
+  const [isDropDownOpen,setIsDropDownOpen] = useState(false)
+  const handleDropDownToogle = () =>{
+    setIsDropDownOpen(!isDropDownOpen)
+  }
+  const userDropdownMenus = [
+    {label:"Dashboard", path:"/dashboard"},
+    {label:"Profile", path:"/dashboard/profile"},
+    {label:"Payments", path:"/dashboard/payments"},
+    {label:"Orders", path:"/dashboard/orders"},
+  ]
+
+  //admin navigation dropdown menu
+  const adminDropdownMenus = [
+    {label:"Dashboard",path:"/dashboard/admin"},
+    {label:"Manage Items",path:"/dashboard/manage-products"},
+    {label:"All Orders",path:"/dashboard/manage-orders"},
+    {label:"Add Product",path:"/dashboard/add-product"},
+  ]
+  //role based dropdown show
+  const dropDownMenus = user?.role === "admin"? [...adminDropdownMenus]:[...userDropdownMenus]
   return (
     <header className="fixed-nav-bar w-nav">
     <nav
@@ -24,9 +48,37 @@ const Navbar = () => {
         <span><button className="hover:text-[#ed3849]">
             <i className="ri-shopping-bag-line"></i><sup
               className="text-sm inline-block w-4 px-1.5 text-white rounded-full bg-[#ed3849] text-center">0</sup>
-          </button>
-          <Link to={'/login'}> <i className="ri-user-line  rounded-full cursor-pointer"></i>
-          </Link>
+          </button></span>
+          <span>
+            {/* login korle user show korbe */}
+            {
+              user?
+               <>
+              <img
+              onClick={handleDropDownToogle}
+               src={user?.profileImage||avatarImg} alt=""  className="size-6 rounded-full cursor-pointer"/>
+               {
+                isDropDownOpen && (
+                  <div className="absolute right-0 mt-3 w-48 bg-white p-4 border-gray-200 rounded-b-lg shadow-lg z-50">
+                    <ul>
+                      {
+                        dropDownMenus.map((menu,index)=>(
+                          <li key={index}>
+                            <Link
+                             onClick={()=> handleDropDownToogle(false)}
+                            to={menu.path}>
+                            {menu.label}
+                            </Link>
+                          </li>
+                        ))
+                      }
+                    </ul>
+                  </div>
+                )
+               }
+              </> : <Link to={'/login'}> <i className="ri-user-line  rounded-full cursor-pointer"></i> </Link>
+            }
+          
           </span>
       </div>
     </nav>
