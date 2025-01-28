e5affb57474a53a37681825bffcc266c63b2482a
Riajul Islam <riajulpislam@gmail.com>
2025-01-28 21:19:17 +0600

    get all products and filtering based on query

--- backend/src/products/product.controller.js
+++ backend/src/products/product.controller.js
@@ -23,7 +23,44 @@ const createNewProduct =async (req,res)=>{
         return errorResponse(res,500, "Failed to create new product",error)
     }
 }
+//get all products
+const getAllProducts =async(req,res)=>{
+    try {
+        const {category,color,minPrice,maxPrice,page=1,limit=10}=req.query;
+        const filter = {}
+        if(category && category !== 'all'){
+            filter.category = category;
+        }
+        if(color && color !== 'all'){
+            filter.color = color;
+        }
+        if( minPrice && maxPrice){
+            const min = parseFloat(minPrice)
+            const max = parseFloat(maxPrice)
+            if(!isNAN(min) && !isNAN(max)){
+                filter.price = {$gte:min, $lte:max}
+            }
+        }
+        const skip =(parseInt(page)-1) * parseInt(limit);
+        const totalProducts= await Products.countDocuments(filter);
+        const totalPages = Math.ceil(totalProducts / parseInt(limit))
 
+
+        const products = await Products.find(filter)
+        .skip(skip)
+        .limit(parseInt(limit))
+        .populate('author','email username')
+        return successResponse(res,200,"Products fetched successfully",data={
+            products,
+            totalProducts,
+            totalPages,
+
+        })
+    } catch (error) {
+        return errorResponse(res,500,"Failed to get all products",error)
+    }
+}
 module.exports = {
-    createNewProduct
+    createNewProduct,
+    getAllProducts
 }
--- backend/src/products/product.route.js
+++ backend/src/products/product.route.js
@@ -1,8 +1,13 @@
 const express = require('express')
-const { createNewProduct } = require('./product.controller')
+const { createNewProduct, getAllProducts } = require('./product.controller')
+const verifyToken = require('../middleware/verifyToken')
+const verifyAdmin = require('../middleware/verifyAdmin')
 const router = express.Router()
 
-//create a product
+//create a product (admin)
 router.post("/create-product",createNewProduct)
+
+//router all products
+router.get('/',getAllProducts)
 
 module.exports = router
