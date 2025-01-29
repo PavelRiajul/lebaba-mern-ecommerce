54f9d3974b6788343eabc6979cfddbafe7053a9d
Riajul Islam <riajulpislam@gmail.com>
2025-01-29 15:47:16 +0600

    review api rtk query

--- backend/src/middleware/verifyToken.js
+++ backend/src/middleware/verifyToken.js
@@ -3,8 +3,8 @@ const jwt = require('jsonwebtoken')
 const JWT_SECRET = process.env.JWT_SECRET_KEY
 const verifyToken =(req,res,next)=>{
     try {
-        //const token = req.cookies.token; TODO :uncomment this when done
-        const token = req.headers.authorization?.split(' ')[1]
+        const token = req.cookies.token; //TODO :uncomment this when done
+        //const token = req.headers.authorization?.split(' ')[1]
         //console.log("Token from cookies",token)
        if(!token){
        return   successResponse(res,401,"Unauthorized Access!")
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,12 +2,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authApi  from "./features/auth/authApi";
 import authReducer from "./features/auth/authSlice"
+import productsApi from "./features/products/productsApi";
 export const store = configureStore({
     reducer:{
         [authApi.reducerPath]: authApi.reducer,
         //authSlice use store.js
-        auth:authReducer
+        auth:authReducer,
+        [productsApi.reducerPath]: productsApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(authApi.middleware)
+        getDefaultMiddleware().concat(authApi.middleware,productsApi.middleware)
 })
